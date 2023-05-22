6ce920d32a22dc099a81a67db979a02c812ea1b6
daniil_linevych21 <dan4224rit@gmail.com>
2023-05-22 22:40:27 +0300

    Add feature Observer

--- Game/Game.cs
+++ Game/Game.cs
@@ -1,9 +1,16 @@
+using blackjack.Game;
+using System.IO;
+using System.Numerics;
+
 namespace BlackJack
 {
-  class Game
+  class Game : GameObserver
   {
     public static readonly int PLAYER_COUNT = 2;
     public static readonly int CARDS_WITHOUT_CONFIRMATION_COUNT = 2;
+    public static readonly string pathToLoseNotifications = "C:\\Users\\HP\\SoftwareDesign\\lab6\\task3\\blackjack\\Game\\ObserverResults\\LoseNotifications.txt";
+    public static readonly string pathToAvarageSum = "C:\\Users\\HP\\SoftwareDesign\\lab6\\task3\\blackjack\\Game\\ObserverResults\\AvarageSumOfPoints.txt";
+    List<float> playersSum = new List<float>();
     private GameState _state = new GameState();
     private List<Player> _createPlayers()
     {
@@ -22,7 +29,10 @@ namespace BlackJack
     }
     private void _initiateState()
     {
+      File.WriteAllText(pathToLoseNotifications, string.Empty);
+      File.WriteAllText(pathToAvarageSum, string.Empty);
       this._state.SetPlayers(this._createPlayers());
+            
     }
     public void Start()
     {
@@ -42,6 +52,7 @@ namespace BlackJack
 
     public void End()
     {
+      WriteAnalytics(CountAvarageSum(playersSum));
       List<Player> winners = this._state.GetWinners();
       Logger.EndGame(winners);
     }
@@ -57,6 +68,38 @@ namespace BlackJack
       {
         player.DrawCard(this._state.Deck);
       }
+      if (PointsCounter.CountSum(player.DrawnCards) > PointsCounter.MAX_POINTS_COUNT)
+      {
+         WriteExaggerate(player, PointsCounter.CountSum(player.DrawnCards));
+      }
+      playersSum.Add(PointsCounter.CountSum(player.DrawnCards));
+
     }
-  }
+
+        public float CountAvarageSum(List<float> playersSum)
+        {
+            float sum = 0;
+            foreach(float _sum in playersSum)
+            {
+                sum += _sum;
+            }
+            return sum/2;
+        }
+
+        public void WriteExaggerate(Player player, int sum)
+        {
+            using (StreamWriter writer = new StreamWriter(pathToLoseNotifications, true))
+            {
+                writer.WriteLine($"Player {player.Name} has {sum} points");
+            }
+        }
+
+        public void WriteAnalytics(float sum)
+        {
+            using (StreamWriter writer = new StreamWriter(pathToAvarageSum, true))
+            {
+                writer.WriteLine($"Avarage sum of players is {sum}");
+            }
+        }
+    }
 }
---
+++ Game/GameObserver.cs
@@ -0,0 +1,16 @@
+﻿using BlackJack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blackjack.Game
+{
+    internal interface GameObserver
+    {
+        public void WriteExaggerate(Player player, int sum);
+
+        public void WriteAnalytics(float sum);
+    }
+}
