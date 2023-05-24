7acca9d459d552a19dbc0b80fd72a0989583c283
Blokha Yaroslav <ipz213_byas@student.ztu.edu.ua>
2023-05-24 22:43:14 +0300

    Strategy

---
+++ Game/Strategy/BotPlayer.cs
@@ -0,0 +1,19 @@
+﻿using BlackJack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blackjack.Game.Strategy
+{
+    class BotPlayer : Player, IBotPlayer
+    {
+        public IStrategy Strategy { get; set; }
+
+        public BotPlayer(string name, IStrategy strategy) : base(name)
+        {
+            Strategy = strategy;
+        }
+    }
+}
